df7ea4e6a72fd2b3ceb19f018e4dd7a997b4c881
fatimamahate <>
2023-04-29 16:56:50 +0000

    add models

--- study_app/models.py
+++ study_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Assignment(models.Model):
+    title = models.CharField(max_length=75, null=False, blank=False)
+    tutor = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='created_assignments')
+    tutee = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='assignments_todo')
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
